c78f2359c31f5fc64e5059b2594f4011a2b1a5a4
ohkubo <ohkubo@ist.aichi-pu.ac.jp>
2018-12-08 00:20:45 +0900

    2017 day4

---
+++ 2017/day4/ver1.hs
@@ -0,0 +1,16 @@
+import Data.List
+
+main = do
+  fi <- readFile "input.txt"
+  let ps = map words $ lines fi
+  let ps1 = filter valid ps
+  print (length ps1)
+  let ps2 = filter valid $ map (map sort) ps1
+  print (length ps2)
+
+valid [] = True
+valid (x:xs) = notElem x xs && valid xs
+
+{-
+今までで一番簡単だったね。
+-}
