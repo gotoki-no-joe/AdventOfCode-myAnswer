7945eb83383991c9b9a85f82eb4bad4262fc1adf
Hirotaka Ohkubo <ohkubo@ist.aichi-pu.ac.jp>
2019-12-05 17:40:17 +0900

    fix and refine

--- 2019/day5/ver1.hs
+++ 2019/day5/ver1.hs
@@ -18,7 +18,7 @@ interpret :: Int -> Array Int Integer -> [Integer] -> [Integer]
 interpret pc mem js = case opcode of
     1 -> arith (+)
     2 -> arith (*)
-    3 -> interpret (pc + 2) (mem // [(fromIntegral $ ma (pc + 1), head js)]) (tail js)
+    3 -> interpret (pc + 2) (memwrite (ma (pc + 1)) (head js)) (tail js)
     4 -> trace (show av1) $ av1 : interpret (pc + 2) mem js
 -- new in part2
     5 -> jump (av1 /= 0)
@@ -36,10 +36,11 @@ interpret pc mem js = case opcode of
     ma a = mem ! (fromIntegral a)
     av1 = (if am1 == 0 then ma else id) (ma (pc + 1))
     av2 = (if am2 == 0 then ma else id) (ma (pc + 2))
-    arith op = interpret (pc + 4) (mem // [(fromIntegral $ ma (pc + 3), op av1 av2)]) js
+    arith op = interpret (pc + 4) (memwrite (ma (pc + 3)) (op av1 av2)) js
     jump True  = interpret (fromIntegral av2) mem js
     jump False = interpret (pc + 3) mem js
-    set1 b = interpret (pc + 4) (mem // [(fromIntegral $ ma (pc +3),if b then 1 else 0)]) js
+    set1 b = interpret (pc + 4) (memwrite (ma (pc + 3)) (if b then 1 else 0)) js
+    memwrite a v = mem // [(fromIntegral a, v)]
 {-
 *Main> main
 0
