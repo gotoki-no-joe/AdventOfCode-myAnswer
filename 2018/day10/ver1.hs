2df35a222ed650b624dacc20cd2a06ae1e13729f
Hirotaka Ohkubo <ohkubo@ist.aichi-pu.ac.jp>
2019-12-20 22:32:55 +0900

    2018 day10 revised

--- 2018/day10/ver1.hs
+++ 2018/day10/ver1.hs
@@ -1,86 +1,88 @@
 import Data.Array
 
 {-
-
-まずはファイルを読んで座標の追跡を実行するところは作れる。
-全部表示するときりがなさそうなので、
-座標のそれぞれの幅での最小値とその時刻を算出してみよう。
-
-ファイルの桁が揃っているので、固定位置で読んでしまえ。
-
+ファイル形式は桁が揃っているので決め打ちで読み込める。
+position=<-31667, -52902> velocity=< 3,  5>
+.........|.........|.........|.........|..
 -}
-
-main = do
-  fi <- readFile "input.txt"
-  let (ps,vs) = unzip $ map readem $ lines fi
-  let img = compute ps vs
-  mapM print img
-
--- 最小サイズの分析
---  let xdmin = compute ps vs
---  print xdmin
-
-readem :: String -> ((Int,Int),(Int,Int))
-readem cs = ((read x, read y),(read p, read q)) where
-  (x,cs1) = splitAt 6 (drop 10 cs)
-  (y,cs2) = splitAt 6 (drop 2 cs1)
-  (p,cs3) = splitAt 2 (drop 12 cs2)
-  q = take 2 (drop 2 cs3)
-
-stepStar :: [(Int,Int)] -> [(Int,Int)] -> [(Int,Int)]
-stepStar ps qs = zipWith add ps qs where
-  add (x,y) (p,q) = (x+p, y+q)
-
-{-
-最小サイズの分析
-
-compute ps vs = minimum $ flip zip [10000..] $ take 10000 $ map getXd $ drop 10000 $ iterate (stepStar vs) ps
+read4 :: String -> ((Int,Int),(Int,Int))
+read4 cs = ((get 10 6, get 18 6), (get 36 2, get 40 2))
   where
-    getXd ps = x2 - x1 where
---      xs = map fst ps
-      xs = map snd ps
-      x1 = minimum xs
-      x2 = maximum xs
+    get a b = read $ take b $ drop a cs
 
--}
-
-{-
-X方向は
-*Main> main
-(61,10619)
-と判明
-Y方向も
-*Main> main
-(9,10619)
-と判明。これだけ作ればいい。
--}
-
--- 画像出力
-compute ps vs = [ [ arr ! (x,y) | x <- [0..dx] ] | y <- [0..dy] ] where
-   ps1 = (!! 10619) $ iterate (stepStar vs) ps
-   x0 = minimum (map fst ps1)
-   x1 = maximum (map fst ps1)
-   dx = x1-x0+1
-   y0 = minimum (map snd ps1)
-   y1 = maximum (map snd ps1)
-   dy = y1-y0+1
-   arr = accumArray ovw '.' ((0,0),(dx,dy)) [((x-x0,y-y0),'#')| (x,y) <- ps1]
---   ovw _ c = c
-   ovw = flip const
+step1 = do
+  fi <- readFile "input.txt"
+  let (ps,vs) = unzip $ map read4 $ lines fi
+  print $ head ps
+  print $ last vs
 
 {-
-*Main> main
-"..##....#####....####...#....#.....###..#####...#....#..######."
-".#..#...#....#..#....#..#....#......#...#....#..#....#..#......"
-"#....#..#....#..#........#..#.......#...#....#...#..#...#......"
-"#....#..#....#..#........#..#.......#...#....#...#..#...#......"
-"#....#..#####...#.........##........#...#####.....##....#####.."
-"######..#....#..#..###....##........#...#....#....##....#......"
-"#....#..#....#..#....#...#..#.......#...#....#...#..#...#......"
-"#....#..#....#..#....#...#..#...#...#...#....#...#..#...#......"
-"#....#..#....#..#...##..#....#..#...#...#....#..#....#..#......"
-"#....#..#####....###.#..#....#...###....#####...#....#..#......"
-"..............................................................."
-[(),(),(),(),(),(),(),(),(),(),()]
+メッセージが現れる瞬間は、星がもっとも密集していると仮定する。
+X,Y座標についてそれぞれ、そのような時刻と振れ幅を算出してみる。
+
+しかし、いずれは星は離れて行ってしまうので、区間で分析する必要がある。
+10000世代単位で調べる。
+-}
+
+step2 k = do
+    fi <- readFile "input.txt"
+    let (ps,vs) = unzip $ map read4 $ lines fi
+    putStr "X axis : "
+    let ans = compute k (map fst ps) (map fst vs)
+    print ans
+    putStr "Y axis : "
+    let ans = compute k (map snd ps) (map snd vs)
+    print ans
+
+compute k ps vs = minimum $ flip zip [k10000..] $ map spectrum $ take 10000 $
+                  iterate (zipWith (+) vs) $ zipWith (+) ps $ map (k10000 *) vs
+  where
+    k10000 = k * 10000
+    spectrum xs = maximum xs - minimum xs
+
+{-
+*Main> step2 0
+X axis : (6259,9999)
+Y axis : (6209,9999)
+*Main> step2 1
+X axis : (61,10619)
+Y axis : (9,10619)
+*Main> step2 2
+X axis : (93869,20000)
+Y axis : (93819,20000)
+
+0-9999ではまだ減少途中。2000-では既に発散。10619がビンゴ。
+-}
+
+step3 = do
+    fi <- readFile "input.txt"
+    let (ps,vs) = unzip $ map read4 $ lines fi
+    let msg = compute2 ps vs
+    putStrLn msg
+
+compute2 :: [(Int,Int)] -> [(Int,Int)] -> String
+compute2 ps vs = unlines [[arr ! (x,y) | x <- [0..dx] ] | y <- [0..dy]]
+  where
+    t = 10619
+    f ps vs = zipWith (+) ps $ map (t *) vs
+    xs = f (map fst ps) (map fst vs)
+    ys = f (map snd ps) (map snd vs)
+    (x0,x1,dx) = (minimum xs, maximum xs, x1 - x0)
+    (y0,y1,dy) = (minimum ys, maximum ys, y1 - y0)
+    arr = accumArray (flip const) '.' ((0,0),(dx,dy)) [((x - x0, y - y0), '#') | (x,y) <- zip xs ys]
+
+{-
+*Main> step3
+..##....#####....####...#....#.....###..#####...#....#..######
+.#..#...#....#..#....#..#....#......#...#....#..#....#..#.....
+#....#..#....#..#........#..#.......#...#....#...#..#...#.....
+#....#..#....#..#........#..#.......#...#....#...#..#...#.....
+#....#..#####...#.........##........#...#####.....##....#####.
+######..#....#..#..###....##........#...#....#....##....#.....
+#....#..#....#..#....#...#..#.......#...#....#...#..#...#.....
+#....#..#....#..#....#...#..#...#...#...#....#...#..#...#.....
+#....#..#....#..#...##..#....#..#...#...#....#..#....#..#.....
+#....#..#####....###.#..#....#...###....#####...#....#..#.....
+
 ABGXJBXF
 -}
